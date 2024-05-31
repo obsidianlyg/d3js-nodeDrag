575a33fe9abcc13070fad402f284d11ebeb4eb0d
obsidianlyg <879142964@qq.com>
2024-05-31 13:56:09 +0800

    init

---
+++ test.js
@@ -0,0 +1,199 @@
+var jsonData = {
+    "nodes": [
+        {
+            "id": "ec61a8f4-73ec-46fe-adb2-4473119a006c",
+            "name": "init",
+            "type": "init",
+            "x": 192,
+            "y": 175.7142791748047,
+            "objId": "99640b9f-ed00-4059-a545-08c0ce851de0"
+        },
+        {
+            "id": "8f45000b-e6f9-4a87-84b7-f1b08319844a",
+            "name": "Node2",
+            "type": "pole",
+            "x": 292.0000228881836,
+            "y": 176.7142791748047,
+            "objId": "2847ceec-93d0-4611-8389-134640fd691a"
+        },
+        {
+            "id": "b65a98ec-4143-4fe0-b382-57b3287406fc",
+            "name": "Node3",
+            "type": "pole",
+            "x": 412.00000762939453,
+            "y": 179.7142791748047,
+            "objId": "0d7c2836-4624-4623-8f61-602864521091"
+        },
+        {
+            "id": "9e5d04c8-561f-4d5a-ab11-99ad4aa8f224",
+            "name": "Node4",
+            "type": "pole",
+            "x": 507.9999771118164,
+            "y": 177.7142791748047
+        },
+        {
+            "id": "7b978d11-b914-4288-87cd-16158034e0eb",
+            "name": "Node5",
+            "type": "meterBox",
+            "x": 352.00000762939453,
+            "y": 267.7142791748047,
+            "objId": "1004ca7e-77f3-4891-b6db-7706673fb7e5"
+        },
+        {
+            "id": "b00e2768-53bb-4c62-8e37-a66c8225deb4",
+            "name": "Node6",
+            "type": "meterBox",
+            "x": 352.00000762939453,
+            "y": 218.7142791748047,
+            "objId": "55b7c65a-12a2-4ced-bec1-f4055192fcfc"
+        },
+        {
+            "id": "f442d9cd-098c-40aa-afde-f308a70626ff",
+            "name": "Node7",
+            "type": "pole",
+            "x": 58.00000762939453,
+            "y": 79.71429443359375,
+            "objId": "2ad6d8a6-1e25-40e5-9fb0-976cd3ee3b2c"
+        },
+        {
+            "id": "6833f32e-be38-4306-9c72-c6e4892e3909",
+            "name": "Node8",
+            "type": "pole",
+            "x": 172,
+            "y": 81.71429443359375,
+            "objId": "0346344d-0258-479d-93ae-15ce0c52028f"
+        },
+        {
+            "id": "93cd42ae-07ad-4afa-ae8b-07cba3ec0432",
+            "name": "Node9",
+            "type": "pole",
+            "x": 292.00001525878906,
+            "y": 81.71429443359375,
+            "objId": "592b928c-4c09-4a62-98d5-8121a095758a"
+        },
+        {
+            "id": "14eb6b52-6185-444b-b04c-238037f84c2e",
+            "name": "Node10",
+            "type": "pole",
+            "x": 412.99998474121094,
+            "y": 81.71427917480469,
+            "objId": "f7a83c39-293d-4e78-8398-8cc92254ee98"
+        },
+        {
+            "id": "db39d4be-a5eb-4f1c-879a-44fa93872e60",
+            "name": "Node11",
+            "type": "pole",
+            "x": 548.0000152587891,
+            "y": 80.71429443359375,
+            "objId": "2030e33b-bf51-4693-a1ce-02c0972211d2"
+        },
+        {
+            "id": "85feec3a-87ce-4d61-8254-1074d4db3c01",
+            "name": "Node12",
+            "type": "pole",
+            "x": 640.0000152587891,
+            "y": 79.71429443359375
+        }
+    ],
+    "links": [
+        {
+            "objId": "ec61a8f4-73ec-46fe-adb2-4473119a006c",
+            "source": "ec61a8f4-73ec-46fe-adb2-4473119a006c",
+            "target": "8f45000b-e6f9-4a87-84b7-f1b08319844a",
+            "color": "#7f8c8d"
+        },
+        {
+            "objId": "8f45000b-e6f9-4a87-84b7-f1b08319844a",
+            "source": "8f45000b-e6f9-4a87-84b7-f1b08319844a",
+            "target": "b00e2768-53bb-4c62-8e37-a66c8225deb4",
+            "color": "#7f8c8d"
+        },
+        {
+            "objId": "b00e2768-53bb-4c62-8e37-a66c8225deb4",
+            "source": "b00e2768-53bb-4c62-8e37-a66c8225deb4",
+            "target": "b65a98ec-4143-4fe0-b382-57b3287406fc",
+            "color": "#7f8c8d"
+        },
+        {
+            "objId": "b65a98ec-4143-4fe0-b382-57b3287406fc",
+            "source": "b65a98ec-4143-4fe0-b382-57b3287406fc",
+            "target": "9e5d04c8-561f-4d5a-ab11-99ad4aa8f224",
+            "color": "#b0cd1d"
+        },
+        {
+            "objId": "7b978d11-b914-4288-87cd-16158034e0eb",
+            "source": "7b978d11-b914-4288-87cd-16158034e0eb",
+            "target": "b00e2768-53bb-4c62-8e37-a66c8225deb4",
+            "color": "#7f8c8d"
+        },
+        {
+            "objId": "f442d9cd-098c-40aa-afde-f308a70626ff",
+            "source": "f442d9cd-098c-40aa-afde-f308a70626ff",
+            "target": "6833f32e-be38-4306-9c72-c6e4892e3909",
+            "color": "#1e90ff"
+        },
+        {
+            "objId": "6833f32e-be38-4306-9c72-c6e4892e3909",
+            "source": "6833f32e-be38-4306-9c72-c6e4892e3909",
+            "target": "93cd42ae-07ad-4afa-ae8b-07cba3ec0432",
+            "color": "#1e90ff"
+        },
+        {
+            "objId": "93cd42ae-07ad-4afa-ae8b-07cba3ec0432",
+            "source": "93cd42ae-07ad-4afa-ae8b-07cba3ec0432",
+            "target": "14eb6b52-6185-444b-b04c-238037f84c2e",
+            "color": "#1e90ff"
+        },
+        {
+            "objId": "14eb6b52-6185-444b-b04c-238037f84c2e",
+            "source": "14eb6b52-6185-444b-b04c-238037f84c2e",
+            "target": "db39d4be-a5eb-4f1c-879a-44fa93872e60",
+            "color": "#1e90ff"
+        },
+        {
+            "objId": "db39d4be-a5eb-4f1c-879a-44fa93872e60",
+            "source": "db39d4be-a5eb-4f1c-879a-44fa93872e60",
+            "target": "85feec3a-87ce-4d61-8254-1074d4db3c01",
+            "color": "#1e90ff"
+        },
+        {
+            "objId": "8f45000b-e6f9-4a87-84b7-f1b08319844a",
+            "source": "8f45000b-e6f9-4a87-84b7-f1b08319844a",
+            "target": "93cd42ae-07ad-4afa-ae8b-07cba3ec0432",
+            "color": "#1e90ff"
+        },
+        {
+            "objId": "b65a98ec-4143-4fe0-b382-57b3287406fc",
+            "source": "b65a98ec-4143-4fe0-b382-57b3287406fc",
+            "target": "14eb6b52-6185-444b-b04c-238037f84c2e",
+            "color": "#1e90ff"
+        }
+    ],
+    "categories": [
+
+    ],
+    "itemStyle": {
+
+    },
+    "label": {
+
+    },
+    "lineStyle": {
+
+    },
+    "roam": false,
+    "draggable": false,
+    "focusNodeAdjacency": false,
+    "layoutIterations": 0,
+    "layoutAnimation": true,
+    "emphasis": {
+
+    },
+    "rippleEffect": {
+
+    },
+    "symbolSize": 10,
+    "symbol": [
+
+    ]
+}
